6eb73e822fb75443ec924c338128f95ea831219f
Nate Hogan <natek.hogan@gmail.com>
2021-04-06 16:40:13 -0600

    added function to connect to database

--- server.js
+++ server.js
@@ -1,9 +1,28 @@
+const mysql = require ('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
+
+// Connect to the database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //your mysql username
+        user: 'root',
+        //your mysql password
+        password: 'Squarepants1!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 app.get('/', (req, res) => {
